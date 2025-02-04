ce44ef93bdc7970040cb2c523b610afd6b355387
jeliaaa <julia.aleqsandre@gmail.com>
2025-02-05 00:18:45 +0400

    a

--- src/components/LanguageDropdown.tsx
+++ src/components/LanguageDropdown.tsx
@@ -1,31 +1,42 @@
 import React from 'react';
-import i18next from 'i18next';
+// import i18next from 'i18next';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
+import enFlag from "../assets/flags/en.png"
+import kaFlag from "../assets/flags/ka.jpg"
+
 
 const LanguageDropdown: React.FC = () => {
     const { i18n } = useTranslation();
     const location = useLocation();
-    const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const selectedLanguage = e.target.value;
-        i18n.changeLanguage(selectedLanguage);
+    const handleLanguageChange = (language: string) => {
+        i18n.changeLanguage(language);
         const currentPath = location.pathname;
         const pathWithoutLanguage = currentPath.replace(/^\/(en|ka|ru)/, '');
-        const newPath = `/${selectedLanguage}${pathWithoutLanguage}`;
-        window.location.href = newPath
+        const newPath = `/${language}${pathWithoutLanguage}`;
+        window.location.href = newPath;
     };
 
     return (
-        <div className="relative w-[100px] text-black">
-            <select
-                value={i18next.language}
-                onChange={handleLanguageChange}
-                className="px-3 py-2 border rounded-md bg-white shadow focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-                <option value="en">EN</option>
-                <option value="ka">KA</option>
-                <option value="ru">RU</option>
-            </select>
+        <div className="relative flex space-x-2">
+            <img
+                src={enFlag}
+                alt="EN"
+                className="w-8 cursor-pointer"
+                onClick={() => handleLanguageChange('en')}
+            />
+            <img
+                src={kaFlag}
+                alt="KA"
+                className="w-8 cursor-pointer"
+                onClick={() => handleLanguageChange('ka')}
+            />
+            {/* <img
+                src="/path/to/flags/ru.png"
+                alt="RU"
+                className="w-8 h-8 cursor-pointer"
+                onClick={() => handleLanguageChange('ru')}
+            /> */}
         </div>
     );
 };
