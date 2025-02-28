4e95deeb301cc3f54cf67c6fa6da71421bb701b5
NikaShabashvili03 <shabashvilinika@yahoo.com>
2025-02-28 05:31:40 +0400

    profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -34,21 +34,37 @@ const Profile = () => {
 
     console.log(data);
 
-
+//     div key={i} className="block border rounded-lg shadow-lg hover:shadow-xl transition">
+//     <div className="w-full animate-pulse aspect-video bg-gray-300" />
+//     <div className="w-full bg-white p-4">
+//         123
+//         {/* <h3 className="text-lg font-semibold">{project.title}</h3> */}
+//         {/* <p className="text-sm text-gray-600">{project.description}</p> */}
+//     </div>
+// </div>
     return (
         <div className="container mx-auto flex flex-col items-center px-4 py-8">
             <div className="bg-main-color rounded-full z-10 text-grayish border-4 border-main-color py-5 px-20 mb-10 text-2xl">
                 {t("myProjects")}
             </div>
             <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {projects.map((project) => (
-                    <Link to={`${project.id}`} key={project.id} className="block border rounded-lg shadow-lg hover:shadow-xl transition">
-                        <img src={karkasi} alt="..." className="" />
+                {!isLoading && data.map((project) => (
+                    <Link to={`${project.track}`} key={project.id} className="block border rounded-lg shadow-lg hover:shadow-xl transition">
+                        <img src={karkasi} alt="..." className="w-full aspect-video bg-gray-300" />
                         <div className="w-full bg-white p-4">
-                            <h3 className="text-lg font-semibold">{project.title}</h3>
-                            <p className="text-sm text-gray-600">{project.description}</p>
+                            <h3 className="text-lg font-semibold">Title</h3>
+                            <p className="text-sm text-gray-600">Desc</p>
                         </div>
                     </Link>
+                ))}
+                {isLoading && new Array(4).fill(0).map((_, i) => (
+                    <div key={i} className="block border rounded-lg shadow-lg hover:shadow-xl transition">
+                        <div className="w-full animate-pulse aspect-video bg-gray-300" />
+                        <div className="w-full bg-white flex flex-col gap-2 p-4">
+                            <div className="text-lg font-semibold animate-pulse w-2/4 h-5 bg-gray-300 rounded-full"></div>
+                            <div className="text-lg font-semibold animate-pulse w-1/4 h-5 bg-gray-300 rounded-full"></div>
+                        </div>
+                    </div>
                 ))}
             </div>
             <div>
