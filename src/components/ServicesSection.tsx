fe299f192b51070b0630fa026cba8c46df9e3f22
NikaShabashvili03 <shabashvilinika@yahoo.com>
2025-03-06 16:29:43 +0400

    tr

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -17,7 +17,7 @@ const ServicesSection = (props: Props) => {
             <div className='flex flex-wrap md:flex-row flex-col gap-y-5 items-center justify-around w-full'>
                 {mockUpData.map((service) => (
                     <Link
-                        className='relative cursor-custom px-14 flex items-center bg-blend-multiply justify-center rounded-lg border-2 border-grayish h-[250px] bg-center bg-cover bg-no-repeat w-[70%] md:w-[30%] hover:border-main-color hover:-translate-y-2 transition-all delay-75'
+                        className='relative cursor-custom lg:px-14 flex items-center bg-blend-multiply justify-center rounded-lg border-2 border-grayish h-[250px] bg-center bg-cover bg-no-repeat w-[70%] md:w-[30%] hover:border-main-color hover:-translate-y-2 transition-all delay-75'
                         to={`/services/${service.id}`}
                         key={service.id}
                         style={{ backgroundImage: `url(${serviceImg})`, backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
