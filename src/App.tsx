45afcb60d12931c00206f6f2c19cad08cb837bff
jeliaaa <julia.aleqsandre@gmail.com>
2025-04-25 22:52:54 +0400

    a

--- src/App.tsx
+++ src/App.tsx
@@ -20,10 +20,14 @@ import ProjectSingle from './pages/ProjectSingle';
 import { useAuth } from './context/AuthContext';
 import ClientOnly from './components/ClientOnly';
 import Loader from './components/Loader';
+import { axiosV2 } from 'utils/axios';
+
 
 const App = () => {
   const location = useLocation();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const pathSegments = location.pathname.split('/');
 
@@ -32,11 +36,18 @@ const App = () => {
     }
   }, [location]);
 
+  useEffect(() => {
+    setLoading(true);
+    axiosV2.get(`/content`)
+      .then(res => setData(res.data))
+      .finally(() => setLoading(false))
+  }, [])
+
   const { status } = useAuth();
 
   const isLoading = useMemo(() => status === 'loading', [status]);
 
-  if (isLoading) {
+  if (isLoading || loading) {
     return <Loader />;
   }
 
@@ -68,7 +79,7 @@ const App = () => {
       w-[80px] h-[80px] md:w-[100px] md:h-[100px] bg-white flex items-center justify-center shadow-2xl shadow-main-color"
       >
         <svg width={35} className='fill-main-color w-[35px] h-[35px] z-50' height={35} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-          <path d="M64 0C28.7 0 0 28.7 0 64L0 352c0 35.3 28.7 64 64 64l96 0 0 80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416 448 416c35.3 0 64-28.7 64-64l0-288c0-35.3-28.7-64-64-64L64 0z"/>
+          <path d="M64 0C28.7 0 0 28.7 0 64L0 352c0 35.3 28.7 64 64 64l96 0 0 80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416 448 416c35.3 0 64-28.7 64-64l0-288c0-35.3-28.7-64-64-64L64 0z" />
         </svg>
       </button>
 
